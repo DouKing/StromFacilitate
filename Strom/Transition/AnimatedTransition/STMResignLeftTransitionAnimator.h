8dbc3d1cd24d5e5c48fff16b0c89f68dfe7e4e73
wuyikai <wuyikai@secoo.com>
2016-07-25 17:23:16 +0800

    ADD: 添加viewController转场动画`Strom/Transition/`

---
+++ Strom/Transition/AnimatedTransition/STMResignLeftTransitionAnimator.h
@@ -0,0 +1,15 @@
+//
+//  STMResignLeftTransitionAnimator.h
+//  StromFacilitate
+//
+//  Created by WuYikai on 16/7/25.
+//  Copyright © 2016年 secoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STMResignLeftTransitionAnimator : NSObject<UIViewControllerAnimatedTransitioning>
+
+@property (nonatomic, assign) UINavigationControllerOperation operation;
+
+@end
