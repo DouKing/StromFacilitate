10b112f66cd4fc8b89e004513efac6ce36cf26e5
wuyikai <wuyikai@secoo.com>
2016-07-27 13:54:57 +0800

    ADD: 添加`STMBaseTransitionAnimator`

---
+++ Strom/Transition/AnimatedTransition/STMBaseTransitionAnimator.h
@@ -0,0 +1,15 @@
+//
+//  STMBaseTransitionAnimator.h
+//  StromFacilitate
+//
+//  Created by WuYikai on 16/7/26.
+//  Copyright © 2016年 secoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STMBaseTransitionAnimator : NSObject<UIViewControllerAnimatedTransitioning>
+
+@property (nonatomic, assign) UINavigationControllerOperation operation;
+
+@end
