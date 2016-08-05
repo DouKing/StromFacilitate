87b1895d0e02cca45c63f9070f67009ff8de4d7a
wuyikai <wuyikai@secoo.com>
2016-08-05 17:07:33 +0800

    MOD: 重新整理`UINavigationController+STM`

--- Strom/UI/ViewControllers/UINavigationController+STM.m
+++ Strom/UI/ViewControllers/UINavigationController+STM.m
@@ -10,11 +10,7 @@
 #import "UIViewController+STM.h"
 #import "STMObjectRuntime.h"
 
-static char * const kSTMCurrentShowVCKey = "kSTMCurrentShowVCKey";
-
-@interface UINavigationController ()<UINavigationControllerDelegate, UIGestureRecognizerDelegate>
-
-@property (nonatomic, weak) UIViewController *currentShowVC;
+@interface UINavigationController ()<UIGestureRecognizerDelegate>
 
 @end
 
@@ -43,43 +39,8 @@ static char * const kSTMCurrentShowVCKey = "kSTMCurrentShowVCKey";
 
 - (instancetype)stm_initWithRootViewController:(UIViewController *)rootViewController {
   typeof(self) nav = [self stm_initWithRootViewController:rootViewController];
-  nav.delegate = self;
   nav.interactivePopGestureRecognizer.delegate = self;
   return nav;
-}
-
-- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-  if (1 == navigationController.viewControllers.count) {
-    self.currentShowVC = nil;
-    return;
-  }
-  self.currentShowVC = viewController;
-}
-
-- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
-  if (gestureRecognizer == self.interactivePopGestureRecognizer) {
-    return (self.currentShowVC == self.topViewController);
-  }
-  return YES;
-}
-
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-  if ([gestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]] &&
-      [otherGestureRecognizer isKindOfClass:[UIScreenEdgePanGestureRecognizer class]]) {
-    return YES;
-  } else {
-    return NO;
-  }
-}
-
-#pragma mark - setter & getter
-
-- (void)setCurrentShowVC:(UIViewController *)currentShowVC {
-  objc_setAssociatedObject(self, kSTMCurrentShowVCKey, currentShowVC, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (UIViewController *)currentShowVC {
-  return objc_getAssociatedObject(self, kSTMCurrentShowVCKey);
 }
 
 @end
