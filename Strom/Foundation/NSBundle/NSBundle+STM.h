ae0da1a6ea3b30ec53ba808f018025564fa433db
WuYikai <wyk8916@gmail.com>
2017-12-26 16:18:12 +0800

    refactor(Foundation): add `NSBundle+STM`

---
+++ Strom/Foundation/NSBundle/NSBundle+STM.h
@@ -0,0 +1,13 @@
+//
+// StromFacilitate
+// NSBundle+STM.h
+// Created by DouKing (https://github.com/DouKing) on 2017/11/27.
+// Copyright © 2017年 secoo. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (STM)
+
++ (NSBundle *)stm_containerAppMainBundle;
+
+@end
---
+++ Strom/Foundation/NSBundle/NSBundle+STM.m
@@ -0,0 +1,20 @@
+//
+// StromFacilitate
+// NSBundle+STM.m
+// Created by DouKing (https://github.com/DouKing) on 2017/11/27.
+// Copyright © 2017年 secoo. All rights reserved.
+
+#import "NSBundle+STM.h"
+
+@implementation NSBundle (STM)
+
++ (NSBundle *)stm_containerAppMainBundle {
+  NSBundle *bundle = [NSBundle mainBundle];
+  if ([[bundle.bundleURL pathExtension] isEqualToString:@"appex"]) {
+    // Peel off two directory levels - MY_APP.app/PlugIns/MY_APP_EXTENSION.appex
+    bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
+  }
+  return bundle;
+}
+
+@end
