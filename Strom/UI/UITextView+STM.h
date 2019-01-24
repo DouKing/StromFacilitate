e9244298bef0a3a26b684e36ff32366ac7610192
wuyikai <wyk8916@gmail.com>
2019-01-24 19:15:52 +0800

    UITextView placeholder

--- Strom/UI/UITextView+STM.h
+++ Strom/UI/UITextView+STM.h
@@ -29,6 +29,13 @@ IB_DESIGNABLE
  */
 @property (nonatomic, assign) IBInspectable NSUInteger limitLength;
 
+@property (nonatomic, readonly) UILabel *placeholderLabel;
+@property (nonatomic, strong) IBInspectable UIColor *placeholderColor;
+@property (nonatomic, strong) IBInspectable NSString *placeholder;
+@property (nonatomic, strong) NSAttributedString *attributedPlaceholder;
+
++ (UIColor *)defaultPlaceholderColor;
+
 @end
 
 NS_ASSUME_NONNULL_END
